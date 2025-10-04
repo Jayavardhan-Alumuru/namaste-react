9eb0da81d6cd9c2caf03ff4721bb682a13cfe0ef
javardhanram@gmail.com <jayavardhan@flick2know.com>
2025-10-04 17:33:01 +0530

    chore: update git ignore

--- episode-03/src/App.js
+++ episode-03/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import { useState } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Header />);
